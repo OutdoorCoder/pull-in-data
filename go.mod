24b10f690228bf30cc13513b67c6567e579e0990
Cameron Massey <cmassey@kochava.com>
2023-02-19 16:24:16 -0800

    chore: go init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OutdoorCoder/read-noaa-data-concurrently
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,5 @@
+package readnoaadataconcurrently
+
+//Find some nature data API's to read from
+//https://www.ncdc.noaa.gov/cdo-web/webservices/v2
+//Base URL: https://www.ncei.noaa.gov/cdo-web/api/v2/{endpoint}
